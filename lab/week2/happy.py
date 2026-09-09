7954a68134bf18cd4c599241e7a322020e361345
taehyeon0123 <eend0831@naver.com>
2026-09-09 13:48:35 +0900

    test_say_happy_birthday3 함수 이름 수정

--- lab/week2/happy.py
+++ lab/week2/happy.py
@@ -12,7 +12,7 @@ def test_say_happy_birthday2():
     for name in nmaes:
         say_happy_birthday(name)
 
-    def test_happy_birthday3():   
+    def test_say_happy_birthday3():   
     say_happy_birthday(3.14)
     say_happy_birthday(100)
     say_happy_birthday([1,2,3])
